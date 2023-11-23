65475d3b1955fb34586a8e0476d90de8ca11d8e5
tens124 <tens124@naver.com>
2023-11-23 09:53:55 +0900

    카테고리 목록에 필요한 컨트롤러, DAO, DTO, 서비스, sql문

---
+++ boss/src/main/java/boss/dao/CategoryDao.java
@@ -0,0 +1,17 @@
+package boss.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import boss.model.Category;
+import boss.model.Product;
+
+@Repository
+public interface CategoryDao {
+
+	int categoryCount(String cid);
+
+	List<Product> categoryList(Category c);
+
+}
